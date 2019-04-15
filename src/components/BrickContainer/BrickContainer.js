002848d50b51b1d7d58df2964702ec7b4445f39a
jeromelimzw <jerome.lim.zw@gmail.com>
2019-04-15 14:40:28 +0800

    add BrickContainer and BrickTable components

---
+++ src/components/BrickContainer/BrickContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Brick from "../Brick/Brick";
+import BrickTable from "../BrickTable/BrickTable";
+
+const BrickContainer = () => {
+  return (
+    <React.Fragment>
+      <Brick />
+      <BrickTable />
+    </React.Fragment>
+  );
+};
+
+export default BrickContainer;
---
+++ src/components/BrickTable/BrickTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const BrickTable = () => {
+  return (
+    <Container>
+      <p className="gray fw7">Explore</p>
+    </Container>
+  );
+};
+
+export default BrickTable;
