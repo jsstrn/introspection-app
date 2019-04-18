83cb0e2ebe2f77cba7eb91aa93ef0bd7043c10ff
nictengkk <nictengkk@gmail.com>
2019-04-18 17:38:54 +0800

    update cypress test for detailed page

--- cypress/integration/routes.spec.js
+++ cypress/integration/routes.spec.js
@@ -9,16 +9,25 @@ describe("Routes", () => {
       .click();
 
     cy.url().should("include", "/home");
+    // cy.get("h1.radar-title").should("contain", "Singapore Introspection Radar");
+    // cy.get("h1.wall-title").should("contain", "Singapore Action Plan");
+    // cy.get("td")
+    // .attr("data-testid", "Diversity and Inclusion-would like to share")
+    // .should("include", 2);
+    // cy.get("td")
+    // .attr("data-testid", "Equitable Tech-would like to deepen")
+    // .should("include", 3);
   });
-
+  
   it("goes to radar page", () => {
     cy.visit("/");
-
+    
     cy.get("h4")
-      .contains("Introspection Radar")
-      .click();
-
+    .contains("Introspection Radar")
+    .click();
+    
     cy.url().should("include", "/radar");
+    cy.get("h1.radar-title").should("contain", "Singapore Introspection Radar");
   });
 
   it("goes to action plan page", () => {
@@ -47,6 +56,7 @@ describe("Routes", () => {
       .contains("Detailed")
       .click();
     cy.url().should("include", "/slice");
+    cy.get("h1").should("include", "Equitable Tech");
   });
 
   it("goes to profile page", () => {
