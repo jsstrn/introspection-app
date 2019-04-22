9d084966189ff836850f3482c28d9238b1523d19
jeromelimzw <jerome.lim.zw@gmail.com>
2019-04-22 15:16:29 +0800

    Replace old upload method with new drag and drop

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -1,80 +1,13 @@
 import { Container } from "reactstrap";
 import React, { Component } from "react";
-import Alert from "react-s-alert";
-
-const isDev = process.env.NODE_ENV !== "production";
-const getUrl = isDev
-  ? "http://localhost:7890"
-  : "https://auto-introspection-api.herokuapp.com";
-
-const alertOpts = {
-  effect: "genie",
-  position: "top-right",
-  timeout: 1500
-};
+import DragAndDropUpload from "../../components/DragAndDropUpload/DragAndDropUpload";
 
 export class AdminPage extends Component {
-  state = {
-    csv: []
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const formData = new FormData();
-    const { csv } = this.state;
-    formData.append("file", csv);
-    try {
-      const res = await fetch(`${getUrl}/upload`, {
-        method: "POST",
-        body: formData,
-        credentials: "include"
-      });
-      // const data = await res.json();
-      if (res.status !== 201) {
-        Alert.warning("File upload failed, please try again", alertOpts);
-      } else {
-        Alert.info("Upload successful", alertOpts);
-        window.setTimeout(() => {
-          this.props.history.push("/");
-        }, 1500);
-      }
-    } catch (error) {
-      Alert.warning("File upload failed, please try again", alertOpts);
-      console.error(error);
-    }
-  };
-
-  handleChange = e => {
-    this.setState({
-      csv: e.target.files[0]
-    });
-  };
-
   renderCsvUploader() {
     return (
       <React.Fragment>
         <h3>🗄 Upload CSV File</h3>
-        <form
-          data-testid="form"
-          action="upload"
-          method="post"
-          encType="multipart/form-data"
-          onSubmit={this.handleSubmit}
-        >
-          <input
-            type="file"
-            ref={input => {
-              this.filesInput = input;
-            }}
-            name="file"
-            accept=".csv"
-            data-testid="uploader"
-            onChange={this.handleChange}
-          />
-          <button type="submit" data-testid="file-submit">
-            Send
-          </button>
-        </form>
+        <DragAndDropUpload history={this.props.history} />
       </React.Fragment>
     );
   }
