12f343d253084a8be6c95003bb5f2142922e42ae
huitian <tanhuitian@gmail.com>
2019-04-22 15:40:58 +0800

    Delete lorem ipsum from Slicedetails

--- src/components/SliceDetails/SliceDetails.jsx
+++ src/components/SliceDetails/SliceDetails.jsx
@@ -7,13 +7,6 @@ function SliceDetails(props) {
       <div className="mx-auto text-center mt-5">
         <h1 className=" text-info font-weight-bolder mb-5">{`${category}`}</h1>
       </div>
-      <p className="mt-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem beatae
-        sint pariatur, delectus harum illo praesentium odit fugiat libero
-        recusandae, corporis totam voluptatum voluptate, deserunt dicta? Itaque
-        expedita sequi consequatur nesciunt dolores nostrum facere nemo vel
-        praesentium, consectetur debitis cum.
-      </p>
     </React.Fragment>
   );
 }
