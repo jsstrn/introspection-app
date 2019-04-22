1dbaa71024af189f1022c4253ad033566e2bb2ce
huitian <tanhuitian@gmail.com>
2019-04-22 15:49:47 +0800

    Remove unused imports from SlicePage

--- src/Pages/SlicePage/SlicePage.jsx
+++ src/Pages/SlicePage/SlicePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
 import HPJumbotron from "../../components/HPJumbotron/HPJumbotron";
 import SliceDetails from "../../components/SliceDetails/SliceDetails";
 import BrickContainer from "../../components/BrickContainer/BrickContainer";
@@ -16,10 +16,7 @@ function SlicePage(props) {
           <SliceDetails category={categoryName} />
         </Row>
         <Row>
-          <Col xs="12" md="6" />
-          <Col xs="12" md="6">
-            <BrickContainer category={categoryName} />
-          </Col>
+          <BrickContainer category={categoryName} />
         </Row>
       </Container>
     </>
