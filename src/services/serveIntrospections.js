bbf93a7d3d44780d54b0509f2afe6c7b8c128e3d
nictengkk <nictengkk@gmail.com>
2019-04-23 18:03:10 +0800

    Add functions to feed data dynamically to Radar.js

--- src/services/serveIntrospections.js
+++ src/services/serveIntrospections.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 import api from './api';
+import { hosts, isValidHost } from "./hosts";
+import { data } from "../tests/seedDataTests";
+const _ = require("lodash");
+
+const host = window.location.host;
+const protocol = window.location.protocol;
+const url = isValidHost(host)
+  ? `${protocol}//${hosts[window.location.host]}`
+  : "";
 
 export const introspectionData = async params => {
   const response = await axios.get(`${api}/${params}`);
   return response.data;
 };
-
 
 const findOne = (data, tarName) => {
   const oneDatum = data.find(a => a.name === tarName);
@@ -65,11 +73,90 @@ const getBrickTable = (data, office, categoryName, actions) => {
   return result;
 };
 
+const getCategories = () => {
+  const categories = data[0].categories.map(a => a.category);
+  return categories;
+};
+
+const oneAngle = () => {
+  return 360 / getCategories().length;
+};
+
+const getNames = () => {
+  return data.map(a => a.name);
+};
+
+const nameArray = () => {
+  const arr = [];
+  getNames(data).forEach(function(element) {
+    _.times(getCategories(data).length, () => arr.push(element));
+  });
+  return arr;
+};
+
+const categoriesAngleArray = () => {
+  const arr = [0];
+  for (let i = 1; i < getCategories(data).length; i++) {
+    arr.push(oneAngle(data) * i);
+  }
+  return arr;
+};
+
+const levelArray = () => {
+  const arr = data.map(a => a.categories.map(a => parseInt(a.level)));
+  return _.flatten(arr);
+};
+
+const levelArrayRandomized = () => {
+  const radius = [];
+  levelArray().forEach(function(element) {
+    switch (element) {
+      case 1:
+        radius.push(_.random(0.7, 1.8));
+        break;
+      case 2:
+        radius.push(_.random(2.3, 3.2));
+        break;
+      case 3:
+        radius.push(_.random(3.5, 4.5));
+        break;
+      case 4:
+        radius.push(_.random(5, 5.8));
+        break;
+      default:
+        radius.push();
+    }
+  });
+  return radius;
+};
+
+const thetaArraySingle = () => {
+  return getCategories(data).map((a, index) =>
+    _.random(index * oneAngle(data) + 10, (index + 1) * oneAngle(data) - 10)
+  );
+};
+
+const thetaArray = () => {
+  const arr = [];
+  data.forEach(function(element) {
+    arr.push(thetaArraySingle(data));
+  });
+  return _.flatten(arr);
+};
+
 export {
   findOne,
   findAllExcept,
   getCategoriesByOffice,
   getBrickElement,
   getBrickTable,
-  getLevelTable
+  getLevelTable,
+  getCategories,
+  oneAngle,
+  getNames,
+  nameArray,
+  categoriesAngleArray,
+  levelArrayRandomized,
+  thetaArraySingle,
+  thetaArray
 };
