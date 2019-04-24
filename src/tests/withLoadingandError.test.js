9dfb256ebce0eda2041affed9ee75e70590eac1d
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-04-24 16:05:24 +0800

    Move test for withLoadingAndError to test folder

--- src/tests/withLoadingandError.test.js
+++ src/tests/withLoadingandError.test.js
@@ -2,7 +2,7 @@ import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
 import React from "react";
 import { render } from "react-testing-library";
-import withLoadingAndError from "./withLoadingAndError";
+import withLoadingAndError from "../components/withLoadingAndError/withLoadingAndError";
 
 describe("withLoadingAndError", () => {
   describe("is loading", () => {
